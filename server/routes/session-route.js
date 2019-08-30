2d173e87aae725f02d9d65d3c9f0996cddb5098d
mujohn26 <52497006+mujohn26@users.noreply.github.com>
2019-08-30 13:51:01 +0200

    #168215558:review a mentor api  (#23)
    * ft(requestsession):add user request session endpoint
    
    add user request session endpoint
    add  user request session test
    
    [finishes #168071264]
    
    * ft(acceptsession):add mentor accept session endpoint
    
    add mentor accept session endpoint
    add  mentor accept session test
    
    [finishes #168071284]
    
    * ft(rejectsession):add mentor reject session endpoint
    
    add mentor reject session endpoint
    add  mentor reject session test
    
    [finishes #168071295]
    
    * ft(reviewmentor):add user review mentor endpoint
    
    add user review mentor endpoint
    add  user review mentor test
    
    [finishes #168215558]

--- server/routes/session-route.js
+++ server/routes/session-route.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import session_controller from '../controllers/session_controller';
+
+import { validcreateSession, validReviewMentor } from '../middleware/sessionValidator';
+import verify from '../middleware/autho';
+
 import { validcreateSession } from '../middleware/sessionValidator';
 import verify from '../middleware/autho';
+
 
 import verifymentor from '../middleware/verifyMentor';
 
 const { verifyMentor } = verifymentor;
-
 
 const { verifyUser } = verify;
 const router = express.Router();
@@ -16,6 +20,8 @@ router.post('/', validcreateSession, verifyUser, session_controller.SessionContr
 
 // accept session
 router.patch('/:sessionid/accept', verifyMentor, session_controller.SessionController.AcceptSession);
+// Reveiw a mentor
+router.post('/:sessionid/review', validReviewMentor, verifyUser, session_controller.SessionController.createReview);
 // decline session
 router.patch('/:sessionid/reject', verifyMentor, session_controller.SessionController.RejectSession);
 
