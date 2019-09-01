3936bdb738f602f20f568d94df7b31eff12185ca
mujohn26 <mujohn26@gmail.com>
2019-09-01 17:56:27 +0200

    push

--- server/middleware/error_handler.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-const errorHandler = (err, req, res, next) => {
-  res.status(500)
-    .send({
-      status: 500,
-      error: 'not Found',
-    });
-};
-export default errorHandler;
