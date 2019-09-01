8055832d64b04677d43e193a1a0606bc0667f1b4
mujohn26 <52497006+mujohn26@users.noreply.github.com>
2019-09-01 17:31:54 +0200

    gh-pages merge (#32)
    * feature(viewmentors):user view mentors page
    
    add a responsive view mentors page
    
    [start #167717021]
    
    * feature(requestmentorship):user request mentorship page
    
    add a request mentorship session page
    
    [start #167717178]
    
    * feature(declinementorship):user decline mentorship page
    
    add a decline  mentorship session page
    
    [start #167717537]
    
    * feature(declinementorship):user decline mentorship page
    
    add a decline  mentorship session page
    
    [start #167717607]
    
    * feature(adminchange):admin change  page
    
    add a page where admin change user to a mentor or a mentor to a user
    
    [start #167717439]

--- UI/js/popup.js
+++ UI/js/popup.js
@@ -6,3 +6,49 @@ var call=(select_obj)=> {
       textArea.style.display = "none";
     }
   }
+
+  var showRow=(row)=>{
+    let rid=Math.floor(Math.random() * 100); 
+      let pop=document.getElementById("popup");
+      let close=document.getElementById("close");
+      row.onclick=function(){pop.style.display="flex";}
+      close.onclick=function(){pop.style.display="none";}
+      let x=row.cells;
+      document.getElementById("sId").innerHTML=rid;
+      document.getElementById("fname").innerHTML=x[0].innerHTML;
+      document.getElementById("lname").innerHTML=x[1].innerHTML;
+      document.getElementById("field").innerHTML=x[2].innerHTML;
+        }
+
+    var out=()=>{
+
+    mname=document.getElementById("mentorName").value;
+    let fname=document.getElementById("fieldName").value;
+    let dt=document.getElementById("startDate").value;
+    let m=document.getElementById("view-pop");
+    let close1=document.getElementById("close1");
+    m.style.display="flex";
+    close1.onclick=function(){
+     m.style.display="none";
+     }                 
+    document.getElementById("mename").innerHTML=mname;
+    document.getElementById("sdate").innerHTML=dt;
+    document.getElementById("field").innerHTML=fname;
+            
+            
+    }  
+    var admin=(row)=>{
+        let pop=document.getElementById("popup");
+        let close=document.getElementById("close");
+        let x=row.cells;
+        row.onclick=function(){
+          if(x[3].innerHTML=="Mentor"){
+          x[4].innerHTML="Changed to user";
+        
+      }
+        else{
+          x[4].innerHTML="Changed to mentor";
+        }
+
+          }
+        }
