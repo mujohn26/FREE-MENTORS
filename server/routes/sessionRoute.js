531c38f4cc9bea2485f68ad11c4aec2d5b7559e8
mujohn26 <52497006+mujohn26@users.noreply.github.com>
2019-09-11 21:38:05 +0200

    #168440065:Ft db accept session  (#81)
    * ft(db-changeuser):change user
    
    admin can change user to a mentor
    
    [start #168365145]
    
    * ft(db-changeuser):change user
    
    changing the response handler in the autho file
    
    [continue #168365145]
    
    * ft(db-viewMentors):view all mentors
    
    a registered user can get all mentors in the database
    
    [start #168367887]
    
    * ft(db-viewMentors):view all mentors
    
    a registered user can get all mentors in the database
    
    [continue #168367887]
    
    * ft(db-viewMentor):view specific mentor
    
    a registered user can get a specific mentor based on the id
    
    [start #168368184]
    
    * ft(db-createSession):create mentorship session
    
    a user can create a mentorship session
    
    [start #168439411]
    
    * ft(db-acceptSession):acceptsession
    
    Mentor accept mentorship session requested to him/her
    
    [start #168440065]

--- server/routes/sessionRoute.js
+++ server/routes/sessionRoute.js
@@ -2,9 +2,12 @@ import express from 'express';
 import sessionController from '../controllers/sessionController';
 import { validcreateSession } from '../middleware/sessionValidator';
 import verify from '../middleware/autho';
+import verifymentor from '../middleware/verifyMentor';
+
+const { verifyMentor } = verifymentor;
 
 const { verifyUser } = verify;
 const router = express.Router();
 router.post('/', validcreateSession, verifyUser, sessionController.SessionController.createSession);
-
+router.patch('/:sessionid/accept', verifyMentor, sessionController.SessionController.AcceptSession);
 export default router;
