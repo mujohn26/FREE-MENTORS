e546e457b736af1c5f725adfe89905691e1d5591
mujohn26 <mujohn26@gmail.com>
2019-09-11 22:06:10 +0200

    ft(db-reviewMentor):review mentor
    A user should review a mentor after a mentorship session
    
    [start #168440949]

--- server/routes/sessionRoute.js
+++ server/routes/sessionRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import sessionController from '../controllers/sessionController';
-import { validcreateSession } from '../middleware/sessionValidator';
+import { validcreateSession, validReviewMentor } from '../middleware/sessionValidator';
 import verify from '../middleware/autho';
 import verifymentor from '../middleware/verifyMentor';
+import reviewController from '../controllers/reviewController';
 
 const { verifyMentor } = verifymentor;
 
@@ -11,4 +12,5 @@ const router = express.Router();
 router.post('/', validcreateSession, verifyUser, sessionController.SessionController.createSession);
 router.patch('/:sessionid/accept', verifyMentor, sessionController.SessionController.AcceptSession);
 router.patch('/:sessionid/reject', verifyMentor, sessionController.SessionController.RejectSession);
+router.post('/:sessionid/review', validReviewMentor, verifyUser, reviewController.ReviewController.createReview);
 export default router;
